d326f293a70b7c30b5b461f25d9ecf63ded95174
Stefan Hagedorn <stefan.hagedorn@tu-ilmenau.de>
2018-09-20 08:14:06 +0200

    improve output for knn

--- stark/kNNQueries.scala
+++ stark/kNNQueries.scala
@@ -44,94 +44,81 @@ object kNNQueries extends App {
 
   
 
-  def warmup(objectRDD: RDD[Index[(STObject, Int)]]) = {
+  def warmup(objectRDD: RDD[Index[(STObject, Int)]], ds: String) = {
     val rangeQueryWindow6 = new Envelope(-180.0, 180.0, -90.0, 90.0)
-    val t0 = System.nanoTime()
-    val warmUpResults = (1 to 20).map(_ => objectRDD.coveredby(rangeQueryWindow6).count()).toList
-    val t1 = System.nanoTime()
-    println("distinct warmup count results ${warmUpResults.distinct.length}")
-    println("warm up took ${(t1 - t0) / 1E9} sec")
+
+    for(i <- 1 to 3) {
+      val t0 = System.nanoTime()
+      val warmUpResults = (1 to 20).map(_ => objectRDD.coveredby(rangeQueryWindow6).count()).toList
+      val t1 = System.nanoTime()
+    }
+
+    println(s"stark;knn;$ds;-;warmup;${(t1 - t0) / 1E9};-;-")
   } 
 
-  def kNN(objectRDD: RDD[Index[(STObject, Int)]]) = {
+  def kNN(objectRDD: RDD[Index[(STObject, Int)]], ds: String) = {
 
     val random = scala.util.Random
 
-    val nQueries = 100
+    val nQueries = 10
     val ks = Seq(1,5,10,20,30,40,50)
     for(k <- ks) {
 
-      println(s"k=$k")
-      
       val t0 = System.nanoTime()
+      var count1 = 0L
       for (i <- 1 to nQueries) {
         var lat = (random.nextDouble() * 2 - 1) * 90
         var long = (random.nextDouble() * 2 - 1) * 180
         val kNNQueryPoint = STObject(long, lat)
-        val count1 = objectRDD.kNN(kNNQueryPoint, k, Distance.seuclid _).count() // flag true to use index
+        count1 = objectRDD.knnAgg(kNNQueryPoint, k, Distance.seuclid _).count() // flag true to use index
       }
       val t1 = System.nanoTime()
-      println("Total Time: " + ((t1 - t0) / 1E9) + " sec")
-      println("Throughput: " + (nQueries * 60) / ((t1 - t0) / (1E9)) + " queries/min")
+      // println("Total Time: " + ((t1 - t0) / 1E9) + " sec")
+      // println("Throughput: " + (nQueries * 60) / ((t1 - t0) / (1E9)) + " queries/min")
+      println(s"stark;knn;$ds;$k;exec;${(t1 - t0) / 1E9};${(nQueries * 60) / ((t1 - t0) / (1E9))};$count1")
     }
   }
 
 
   def knnPoint() {
 
-    println("************************ POINT KNN Queries **************************************")
-
     val objectRDD = sc.textFile(points, numPartitions).map(_.split(',')).map{ arr => (STObject(arr(0).toDouble, arr(1).toDouble), arr(0).toDouble.toInt)}
                       .index(None, RTreeConfig(order = 50))
                       .cache()
 
-    warmup(objectRDD)
-    kNN(objectRDD)    
+    warmup(objectRDD, "point")
+    kNN(objectRDD, "point")    
 
-    println("***********************************************************************************")
-    println("")
   }
 
   def knnLineString() {
 
-    println("************************ LineString KNN Queries **************************************")
     val objectRDD = sc.textFile(linestrings, numPartitions).map{ line => (STObject(line), line.length)}
                       .index(None, RTreeConfig(order = 50))
                       .cache()
 
-    warmup(objectRDD)
-    kNN(objectRDD)
+    warmup(objectRDD, "linestring")
+    kNN(objectRDD, "linestring")
 
-    println("***********************************************************************************")
-    println("")
   }
 
   def knnPolygon() {
-
-    println("************************ POLYGON KNN Queries **************************************")
 
     val objectRDD = sc.textFile(polygons, numPartitions).map{ line => (STObject(line), line.length)}
                       .index(None, RTreeConfig(order = 50))
                       .cache()
 
-    warmup(objectRDD)
-    kNN(objectRDD)
-
-    println("***********************************************************************************")
-    println("")
+    warmup(objectRDD, "polygon")
+    kNN(objectRDD, "polygon")
   }
 
   def knnRectangle() {
-
-    println("************************ Rectangle KNN Queries **************************************")
 
     val objectRDD = sc.textFile(rectangles, numPartitions).map{ line => (STObject(line), line.length)}
                       .index(None, RTreeConfig(order = 50))
                       .cache()
 
-    warmup(objectRDD)
-    kNN(objectRDD)
-    println("***********************************************************************************")
-    println("")
+    warmup(objectRD, "rectangle")
+    kNN(objectRDD, "rectangle")
   }
 }
